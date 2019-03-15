9c6546ff9a9d826847db01d08ff38e073365f2ad
13855140981 <871707394@qq.com>
2019-03-15 16:11:30 +0800

    run

--- jenkins-android/run.sh
+++ jenkins-android/run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-cd /home/android/android-sdk-linux/platforms
-count=`ls $*|wc -w`
-if [ "$count" > "0" ];
-then
- echo y | android update sdk --no-ui
-else
- echo "android is init"
-fi
-
-cd /usr/lib/tomcat/apache-tomcat-8.5.37/bin
-chmod 777 *.sh
-./startup.sh start
+#androidfile="/home/android/android-sdk-linux/platforms"
+#count=`ls $androidfile | wc -w`
+#if [ "$count" > "0" ];
+#then
+#	echo yes | android update sdk --no-ui 
+#else
+# 	echo "android is init"
+#fi 
+#jenksfile="/usr/lib/tomcat/apache-tomcat-8.5.37/webapps/jenkins.war"
+#if [ ! -f "$jenksfile" ]; 
+#then
+# 	wget -P /usr/lib/tomcat/apache-tomcat-8.5.37/webapps http://mirrors.jenkins.io/war/latest/jenkins.war
+#else
+#	echo "jenkins is download"
+#fi
+TOMCAT_HOME="/usr/lib/tomcat/apache-tomcat-8.5.37"  
+chmod 777 /usr/lib/tomcat/apache-tomcat-8.5.37/bin/*.sh
+sleep 5s
+#startup tomcat 
+cd ${TOMCAT_HOME}/bin/
+./catalina.sh start
