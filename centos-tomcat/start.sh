8527b6c7dc8905e590ddf83c779d3cf325f4d201
13855140981 <871707394@qq.com>
2019-04-08 17:28:35 +0800

    修改镜像

---
+++ centos-tomcat/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#nohup /usr/sbin/sshd -D > sshd.log 2>&1 & 
+
+#sh /usr/lib/tomcat/apache-tomcat-8.5.37/bin/catalina.sh run
+
+nohup sh /usr/lib/tomcat/apache-tomcat-8.5.37/bin/catalina.sh run > tomcat.log 2>&1 & 
+
+exec /run.sh
