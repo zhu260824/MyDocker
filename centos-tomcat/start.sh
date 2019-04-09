85a3083a898c67479bfc95b066ee9092ad2cc81c
13855140981 <871707394@qq.com>
2019-04-09 11:58:05 +0800

    删除大文件

--- centos-tomcat/start.sh
+++ centos-tomcat/start.sh
@@ -4,6 +4,6 @@
 
 #sh /usr/lib/tomcat/apache-tomcat-8.5.37/bin/catalina.sh run
 
-nohup sh /usr/lib/tomcat/apache-tomcat-8.5.37/bin/catalina.sh run > tomcat.log 2>&1 & 
+nohup sh /usr/lib/apache-tomcat-8.5.37/bin/catalina.sh run > tomcat.log 2>&1 & 
 
 exec /run.sh
