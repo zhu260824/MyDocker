d3ffd036a1320824776ddd872b6e7123dbbb04e0
朱林 <zl112846@alibaba-inc.com>
2019-06-26 09:34:19 +0800

    xz

---
+++ ubuntu-nginx/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#nohup /usr/sbin/sshd -D > sshd.log 2>&1 & 
+
+#sh /usr/lib/tomcat/apache-tomcat-8.5.37/bin/catalina.sh run
+
+# nohup sh /usr/lib/apache-tomcat-8.5.37/bin/catalina.sh run > tomcat.log 2>&1 & 
+
+service nginx restart
+
+exec /usr/sbin/sshd -D
